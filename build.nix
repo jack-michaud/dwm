6fc6f920d87decb8073680074fdccdf0c455f259
Jack Michaud <jack@lomz.me>
2021-05-25 22:26:28 -0700

    Add dwm-status script

--- build.nix
+++ build.nix
@@ -1,20 +1,28 @@
 { pkgs }:
 with pkgs;
-stdenv.mkDerivation {
-  name = "dwm";
-  src = ./.;
+let
+  dwmStatus = import ./bin/dwm-status.nix { inherit pkgs; };
+in
+  stdenv.mkDerivation {
+    name = "dwm";
+    src = ./.;
 
-  buildInputs = [
-    xorg.libX11
-    xorg.libXft
-    xorg.libXinerama
-    iosevka
-  ];
+    buildInputs = [
+      xorg.libX11
+      xorg.libXft
+      xorg.libXinerama
+      iosevka
+    ];
 
-  installPhase = ''
-    make dwm
-    mkdir -p $out/bin/
-    mv dwm $out/bin/
-  '';
+    propagatedBuildInputs = [
+      dwmStatus
+    ];
 
-}
+    installPhase = ''
+      make dwm
+      mkdir -p $out/bin/
+      cp ${dwmStatus}/bin/dwmStatus $out/bin/
+      mv dwm $out/bin/
+    '';
+
+  }
